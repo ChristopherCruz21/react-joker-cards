22d2127ceeea1a73ead63326a7631f92e420791f
Christopher Cruz Collazo <37718616+nas20@users.noreply.github.com>
2019-11-13 12:34:02 -0400

    Site

---
+++ precache-manifest.8b381112a7714cb21b607891d90d4e3a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8e1ee072a82cca3a2aa38016422f4b82",
+    "url": "/react-joker-cards/index.html"
+  },
+  {
+    "revision": "b5a25163ca89e3c06db4",
+    "url": "/react-joker-cards/static/css/2.b0bb7f2c.chunk.css"
+  },
+  {
+    "revision": "a404d2ea73525c492ae3",
+    "url": "/react-joker-cards/static/css/main.b100e6da.chunk.css"
+  },
+  {
+    "revision": "b5a25163ca89e3c06db4",
+    "url": "/react-joker-cards/static/js/2.808c7678.chunk.js"
+  },
+  {
+    "revision": "a404d2ea73525c492ae3",
+    "url": "/react-joker-cards/static/js/main.b8ecf71f.chunk.js"
+  },
+  {
+    "revision": "e9ad9f84ac2e656a7739",
+    "url": "/react-joker-cards/static/js/runtime-main.ecc36cc3.js"
+  }
+]);
